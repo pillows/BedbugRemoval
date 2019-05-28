6fdf0b6a5752f2b682d5582e4d66778e646993f6
Tommi-Tsuruga <tommi.tsuruga@gmail.com>
2019-05-28 15:27:10 -0400

    Abou added

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import App from "./App";
 import Donate from "./Donate";
+import About from "./About";
 function Routing() {
     return (
       <Router>
@@ -9,6 +10,7 @@ function Routing() {
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/donate" component={Donate} />
+                <Route path="/about" component={About} />
             </Switch>
           
         </div>
